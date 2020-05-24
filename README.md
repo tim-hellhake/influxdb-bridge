2087b32d87d8cdd4d4942e918153457bf9af8213
Tim Hellhake <tim-hellhake@users.noreply.github.com>
2020-05-25 00:15:07 +0200

    Initial release

---
+++ README.md
@@ -0,0 +1,14 @@
+# InfluxDB bridge
+
+[![Build Status](https://travis-ci.org/tim-hellhake/influxdb-bridge.svg?branch=master)](https://travis-ci.org/tim-hellhake/influxdb-bridge)
+[![dependencies](https://david-dm.org/tim-hellhake/influxdb-bridge.svg)](https://david-dm.org/tim-hellhake/influxdb-bridge)
+[![devDependencies](https://david-dm.org/tim-hellhake/influxdb-bridge/dev-status.svg)](https://david-dm.org/tim-hellhake/influxdb-bridge?type=dev)
+[![optionalDependencies](https://david-dm.org/tim-hellhake/influxdb-bridge/optional-status.svg)](https://david-dm.org/tim-hellhake/influxdb-bridge?type=optional)
+[![license](https://img.shields.io/badge/license-MPL--2.0-blue.svg)](LICENSE)
+
+Save your device states to an InfluxDB.
+
+# How to use
+* Go to `settings/developer` and click `Create local authorization`
+* Create a new token and copy it
+* Go to the settings of the influxdb bridge and insert the copied token
---
+++ src/influxdb-bridge.ts
@@ -0,0 +1,99 @@
+/**
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.*
+ */
+
+import { Adapter, } from 'gateway-addon';
+import { WebThingsClient } from 'webthings-client';
+import { client as WebSocketClient } from 'websocket';
+import { InfluxDB } from 'influx';
+
+interface ThingEvent {
+    messageType: string,
+    data: {}
+}
+
+export class InfluxDBBridge extends Adapter {
+    constructor(addonManager: any, private manifest: any) {
+        super(addonManager, InfluxDBBridge.name, manifest.name);
+        addonManager.addAdapter(this);
+        this.connectToinflux();
+    }
+
+    private connectToinflux() {
+        const {
+            host,
+            port,
+            database
+        } = this.manifest.moziot.config;
+
+        console.log(`Connecting to influx at ${host}`);
+
+        const influxdb = new InfluxDB({
+            host,
+            port,
+            database
+        });
+
+        this.connectToGateway(influxdb);
+    }
+
+    private connectToGateway(influxdb: InfluxDB) {
+        console.log('Connecting to gateway');
+
+        const {
+            accessToken
+        } = this.manifest.moziot.config;
+
+        (async () => {
+            const webThingsClient = await WebThingsClient.local(accessToken);
+            const devices = await webThingsClient.getDevices();
+
+            for (const device of devices) {
+                try {
+                    const parts = device.href.split('/');
+                    const deviceId = parts[parts.length - 1];
+
+                    console.log(`Connecting to websocket of ${deviceId}`);
+                    const thingUrl = `ws://localhost:8080${device.href}`;
+                    const webSocketClient = new WebSocketClient();
+
+                    webSocketClient.on('connectFailed', function (error) {
+                        console.error(`Could not connect to ${thingUrl}: ${error}`)
+                    });
+
+                    webSocketClient.on('connect', function (connection) {
+                        console.log(`Connected to ${thingUrl}`);
+
+                        connection.on('error', function (error) {
+                            console.log(`Connection to ${thingUrl} failed: ${error}`);
+                        });
+
+                        connection.on('close', function () {
+                            console.log(`Connection to ${thingUrl} closed`);
+                        });
+
+                        connection.on('message', async function (message) {
+                            if (message.type === 'utf8' && message.utf8Data) {
+                                const thingEvent = <ThingEvent>JSON.parse(message.utf8Data);
+
+                                if (thingEvent.messageType === 'propertyStatus') {
+                                    if (Object.keys(thingEvent.data).length > 0) {
+                                        await influxdb.writeMeasurement(deviceId, [{
+                                            fields: thingEvent.data
+                                        }]);
+                                    }
+                                }
+                            }
+                        });
+                    });
+
+                    webSocketClient.connect(`${thingUrl}?jwt=${accessToken}`);
+                } catch (e) {
+                    console.log(`Could not process device ${device.title} ${e}`);
+                }
+            }
+        })();
+    }
+}
